515ba3ec9ee7075e132ce414dd44f9e1bb7fbd68
Matt367 <vmateoisaias@gmail.com>
2025-08-08 19:16:21 -0300

    Implementación del código JavaScript
    Se inició un array para guardar el nombre de los amigos y una funci´´on para agregar un valor a la lista, tanto como la limpieza del campo y la detección de un valor vacío

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombre = document.getElementById('amigo');
+let amigos = [];
+
+function agrAmi(){
+    let amigo = nombre.value;
+    if (amigo != ''){
+        console.log(amigo);
+        amigos.push(amigo);
+        console.log(amigos);
+        nombre.value = '';
+    }else{
+        alert('No se ingresó ningún nombre.');
+    }
+}
